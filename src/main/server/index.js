0510ae986e41ddfb3348e71a0e4bf2bb6b57dfc3
Elliott Mantock <emantock@egencia.com>
2018-04-20 14:12:24 -0500

    refactored the carousel hbs

--- src/main/server/index.js
+++ src/main/server/index.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const app = express();
 
+// node based requirements
+const {readFile, readFileSync, readdirSync} = require('fs');
+const {join} = require('path');
+
 // view engine
 const Handlebars = require('handlebars');
+
+// app bundles
+const bundleDir = join('dist', 'bundle');
 
 // middleware to make life easier
 const bodyParser = require('body-parser');
@@ -11,23 +18,17 @@ const cookieParser = require('cookie-parser');
 const upload = require('multer')();
 const helmet = require('helmet');
 
-// node based requirements
-const {readFile, readFileSync, readdirSync} = require('fs');
-const {join} = require('path');
-
 // details for logging
 const {hostname} = require('os');
 const {name: application_name, version: build_version} = require(join(__dirname, '..', '..', '..', 'package.json'));
 const build_date = (new Date()).toISOString();
 const machine_name = hostname();
 
-
-// TEMPLATING
+// TEMPLATING - TODO move to another file
 // details for presenting
 const viewsDir = join('src', 'main', 'assets', 'views');
 const layoutsDir = join(viewsDir, 'layouts');
 const partialsDir = join(viewsDir, 'app', 'partials');
-const bundleDir = join('dist', 'bundle');
 
 // register the main partials that app templates use
 Handlebars.registerPartial('layouts/default', readFileSync(join(layoutsDir, 'default.hbs')).toString());
@@ -36,6 +37,13 @@ Handlebars.registerPartial('footer', readFileSync(join(layoutsDir, 'footer.hbs')
 readdirSync(partialsDir).forEach((file) => {
   // partials are registered under the app/partials directory the joined by the partial name
   Handlebars.registerPartial(`app/partials/${file.slice(0, -4)}`, readFileSync(join(partialsDir, file)).toString());
+});
+
+// #range block helper takes a number n as an arg and renders the block n times
+Handlebars.registerHelper('range', function(length, options) {
+  return new Handlebars.SafeString(Array.from({length}, (value, index) => {
+    return options.fn({length, value}, {data: {index}});
+  }).join(''));
 });
 
 // app view engine
@@ -57,7 +65,7 @@ app.set('views', viewsDir);
 app.set('view engine', 'hbs');
 // \TEMPLATING
 
-// MIDDLEWARE
+// MIDDLEWARE - TODO move to another file
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -81,7 +89,7 @@ app.use(function(err, req, res, next) {
 });
 // \MIDDLEWARE
 
-// CONTROLLERS
+// CONTROLLERS - TODO move to another file
 // version endpoint
 app.get('/base/version', function(req, res) {
   res.jsonp({machine_name, build_date, build_version, application_name});
