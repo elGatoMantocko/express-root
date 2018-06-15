d1e4733ad1666b2a3d75e8d6db0c0c2989a02c73
Elliott Mantock <emantock@egencia.com>
2018-06-15 08:37:48 -0500

    change caching strategy

--- src/main/assets/sw.js
+++ src/main/assets/sw.js
@@ -5,10 +5,10 @@ if (workbox) {
 
   workbox.precaching.precacheAndRoute([]);
 
-  workbox.routing.registerRoute( /\/input/, workbox.strategies.staleWhileRevalidate());
-  workbox.routing.registerRoute( /\/home/, workbox.strategies.staleWhileRevalidate());
-  workbox.routing.registerRoute(/\/fonts\/fontawesome-webfont/, workbox.strategies.staleWhileRevalidate());
-  workbox.routing.registerRoute(/\/favicon\.ico/, workbox.strategies.staleWhileRevalidate());
+  workbox.routing.registerRoute( /\/input/, workbox.strategies.networkFirst());
+  workbox.routing.registerRoute( /\/home/, workbox.strategies.networkFirst());
+  workbox.routing.registerRoute(/\/fonts\/fontawesome-webfont/, workbox.strategies.cacheFirst());
+  workbox.routing.registerRoute(/\/favicon\.ico/, workbox.strategies.cacheFirst());
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
