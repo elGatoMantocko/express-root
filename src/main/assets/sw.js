cd9ebf1aff7649c9722f5ddc813feac95dcad73d
Elliott Mantock <emantock@egencia.com>
2018-06-16 17:30:14 -0400

    update to use npm bundled workbox

--- src/main/assets/sw.js
+++ src/main/assets/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+importScripts('/js/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
