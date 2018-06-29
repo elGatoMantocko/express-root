65e794263cd02f14ba89dc6df5f9a821b0780c2a
Elliott Mantock <emantock@egencia.com>
2018-06-29 10:38:07 -0500

    added unit testing framework

---
+++ karma.conf.js
@@ -0,0 +1,54 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+const {JS_FILES} = require('./buildtools/paths.js');
+const {join} = require('path');
+
+module.exports = exports = function(config) {
+  config.set({
+    files: [
+      // setup css
+      'public/css/deps.css',
+      'public/css/styles.css',
+
+      // setup testing environment
+      'src/main/assets/js/unit-tests/helpers/**/*.js',
+
+      // setup defined on page
+      'public/js/deps.js',
+      'public/js/templates.js',
+      ...JS_FILES.map((path) => join('src', 'main', 'assets', 'js', path)),
+
+      // unit tests
+      'src/main/assets/js/unit-tests/spec/**/*.js',
+    ],
+
+    browsers: ['PhantomJS', 'ChromeHeadless'],
+    frameworks: ['mocha', 'chai'],
+
+    reporters: ['mocha', 'coverage'],
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        {type: 'text-summary'},
+      ],
+    },
+    preprocessors: {
+      '**/js/libs/**/*.js': ['babel', 'sourcemap', 'coverage'],
+      '**/js/presenters/**/*.js': ['babel', 'sourcemap', 'coverage'],
+    },
+    babelPreprocessor: {
+      options: {
+        sourceMap: 'inline',
+      },
+      sourceFileName: function(file) {
+        return file.originalPath;
+      },
+    },
+
+    proxies: {
+      '/sw.js': 'public/sw.js',
+    },
+
+    singleRun: true,
+    autoWatch: true,
+  });
+};
